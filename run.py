7915cc78298b9088383bdbeb6edfd21a4ad598ab
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-10-22 15:52:24 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 import os
 
 
+"""
+If you create other roles files, you need to had :
+        os.startfile("filename")
+To run.py
+"""
+
 os.startfile("Raimbow.py")
 os.startfile("Role_1.py")
 os.startfile("Role_2.py")

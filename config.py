4712505abd8490f916aac2a317761fc9a2bd7f65
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-07-14 20:11:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ DELAY = 5
 SERVER_ID = "enter server ID"
 TOKEN = "enter bot token"
 ROLES = ["role1","role2"] # enter role names
+COLOURS = [0xDC143C,0xFF69B4,0xFF1493,0xFF6347,0xFFD700,0xEE82EE,0xBA55D3,0x7FFF00,0x00FFFF,0x4682B4,0xD2691E] # add hex colors

264cdc0b1600b830844b3bd090510c8cae0fb726
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-07-14 12:31:15 +0200

    Update run.py
    deal with all the roles in one file with config etc...

--- run.py
+++ run.py
@@ -1,11 +1,34 @@
-import os
+from config import *
+import discord
+import asyncio
+import random
 
+client = discord.Client(intents=discord.Intents.default())
+colours = [discord.Color.dark_orange(),discord.Color.orange(),discord.Color.dark_gold(),discord.Color.gold(),discord.Color.dark_magenta(),discord.Color.magenta(),discord.Color.red(),discord.Color.dark_red(),discord.Color.blue(),discord.Color.dark_blue(),discord.Color.teal(),discord.Color.dark_teal(),discord.Color.green(),discord.Color.dark_green(),discord.Color.purple(),discord.Color.dark_purple()]
 
-"""
-If you create other roles files, you need to had :
-        os.startfile("filename")
-To run.py
-"""
+async def rainbowroles():
+    guild = await client.fetch_guild(SERVER_ID)
+    for role in guild.roles:
+            if str(role) in ROLES:
+                print("detected role: " + "\033[95m" + str(role) + "\033[0m")
 
-os.startfile("Raimbow.py")
-os.startfile("Role.py")
+    while not client.is_closed():
+        for role in guild.roles:
+            if str(role) in ROLES:
+                try:
+                    await role.edit(color=random.choice(colours))
+                except Exception:
+                    print("\033[31mcan't edit role, make sure the bot role is above the rainbow role and that is have the perms to edit roles\033[0")
+        await asyncio.sleep(DELAY)                            
+
+@client.event
+async def on_ready():
+    client.loop.create_task(rainbowroles())
+    print("\033[32m--------------")
+    print("Logged in as :")
+    print(client.user.name)
+    print(client.user.id)
+    print("Ready.")
+    print("--------------\033[0m")
+
+client.run(TOKEN)
